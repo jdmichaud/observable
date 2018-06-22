2a77530ac6f09dd622681a12b36d7a0a97ba9fe7
jd <jean.daniel.michaud@gmail.com>
2018-06-22 21:18:55 +0200

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  verbose: true,
+  "transform": {
+    "^.+\\.ts$": "ts-jest",
+  },
+  "testRegex": "src/.*\\.spec\\.ts$",
+  "moduleFileExtensions": [
+    "ts",
+    "js",
+  ],
+};
