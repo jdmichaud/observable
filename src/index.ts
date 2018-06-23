61936f319dc445e7100a0523bbd141f97febf5cd
jd <jean.daniel.michaud@gmail.com>
2018-06-23 11:23:31 +0200

    Export all classes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { Observable } from "./observable";
+export { Observer } from "./observer";
+export { IdleSubject, Subject } from "./subject";
+export { SubscriberFunction, Subscription } from "./subscription";
