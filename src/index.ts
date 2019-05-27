9ed45d0e23e05f2da4396113eeeb62209fee18d7
jd <jean.daniel.michaud@gmail.com>
2019-05-27 19:27:17 +0200

    Add ReplaySubject to export list.

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { Observable } from './observable';
 export { Observer } from './observer';
 export { Subject } from './subject';
 export { IdleSubject } from './idle-subject';
+import { ReplaySubject } from './replay-subject';
 export { SubscriberFunction, Subscription } from './subscription';
